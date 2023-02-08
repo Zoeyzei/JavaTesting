45a7c4fe58e6245a3038b7695a2f37be7d632b14
Zoey <827650256@qq.com>
2023-02-08 17:21:03 +0800

    链表函数构造

--- src/com/java/CodeCapriccio/ListNodeTest.java
+++ src/com/java/CodeCapriccio/ListNodeTest.java
@@ -1,38 +1,67 @@
 package com.java.CodeCapriccio;
 
+import sun.security.util.Length;
+
+import javax.swing.*;
+
 public class ListNodeTest {
     int val;
     ListNodeTest next;
-    ListNodeTest() {}
+    int nums [];
+
+
+    ListNodeTest() {
+    }
+
     ListNodeTest(int val) {
         this.val = val;
     }
+
     ListNodeTest(int val, ListNodeTest next) {
         this.val = val;
         this.next = next;
+
     }
 
 
-
-
-    public static void main(String[] args) {
-        ListNodeTest listnode = new ListNodeTest();
-//        head.val = [1,2,6,3,4,5,6];
-        listnode.val = 6;
-        listnode.head = new ListNodeTest();
-        ListNodeTest.removeElements(head,listnode.val);
-    }
-
-    public static ListNodeTest removeElements(ListNodeTest head, int val) {
-        if (head==null){
-            return head;
+    public void addNode(ListNodeTest head, int val) {
+        ListNodeTest newNode = new ListNodeTest(val);
+        if (head == null) {
+            head = newNode;
+            return;
         }
-        head.next = removeElements(head.next,val);
-        return head.val==val ? head.next : head;
-        System.out.println(head);
+        ListNodeTest temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
         }
+        temp.next = newNode;
     }
 
+
+
+
+
+        public void main(String[] args) {
+            ListNodeTest listnode = new ListNodeTest();
+            listnode.nums = new int[]{2, 3, 1, 2, 4, 3};
+            for (int i=0;i< listnode.nums.length;i++){
+                listnode.val = listnode.nums[i];
+                listnode = listnode.next;
+            }
+            System.out.println();
+            listnode.val = 6;
+            ListNodeTest.removeElements(listnode, listnode.val);
+        }
+
+        public static ListNodeTest removeElements(ListNodeTest head, int val) {
+            if (head == null) {
+                return head;
+            }
+            head.next = removeElements(head.next, val);
+            return head.val == val ? head.next : head;
+//        System.out.println(head);
+        }
+    }
 /********************************       移除列表元素       **************************************
     给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
      示例 1：
