810d0aae39b7992aeb0320f85429f65beb8c193e
Zoey <827650256@qq.com>
2023-04-03 17:28:49 +0800

    20230403

---
+++ src/com/java/CodeCapriccio/BinaryTreeTest.java
@@ -0,0 +1,83 @@
+package com.java.CodeCapriccio;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeTest {
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+/**********************     二叉树的所有路径    ********************
+ 输入：root = [1,2,3,null,5]
+ 输出：["1->2->5","1->3"]
+
+深度优先遍历+递归
+*/
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        String s;
+        if (root.left==null && root.right==null){
+            res.add(root);
+        }
+        for (String path : binaryTreePaths(root.left)){
+            res.add(root.val + "->" + path);
+        }
+    }
+
+
+
+/*********************  遍历  ***********************
+ 前序遍历：中左右
+ 中序遍历：左中右
+ 后序遍历：左右中
+*/
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+//  前序遍历
+    void preorder(TreeNode root, List<Integer> list){
+        if (root==null){
+            return;
+        }
+        list.add(root.val);
+        preorder(root.left,list);
+        preorder(root.right,list);
+    }
+//  中序遍历
+    void inorder(TreeNode root, List<Integer> list){
+        if (root==null){
+            return;
+        }
+        preorder(root.left,list);
+        list.add(root.val);
+        preorder(root.right,list);
+    }
+//  后序遍历
+    void postorder(TreeNode root, List<Integer> list){
+        if (root==null){
+            return;
+        }
+        preorder(root.left,list);
+        list.add(root.val);
+        preorder(root.right,list);
+    }
+
+}
--- src/com/java/CodeCapriccio/HashTest.java
+++ src/com/java/CodeCapriccio/HashTest.java
@@ -1,14 +1,14 @@
 package com.java.CodeCapriccio;
 
 import javax.swing.text.html.parser.Entity;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class HashTest {
     int i;
     int[]nums;
     String s,t;
+    Deque<Character> stackin;
+
 
 
     public static void main(String[] args) {
@@ -94,8 +94,61 @@ public class HashTest {
     }
 
 
+/*********************************   有效的括号   ********************************
+ 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ 有效字符串需满足：
+ 左括号必须用相同类型的右括号闭合。
+ 左括号必须以正确的顺序闭合。
+ 每个右括号都有一个对应的相同类型的左括号。
 
+ 示例 1：
+ 输入：s = "()"
+ 输出：true
+ 示例 2：
+ 输入：s = "()[]{}"
+ 输出：true
+ 示例 3：
+ 输入：s = "(]"
+ 输出：false
 
+ 思路：栈，无效括号分为三种情况（左括号多余 ((([{}]))()，右括号多余 ({})[[]]}，左右括号数目一样但是不匹配 [[{}]](}）
+ 将元素依次进栈，比较栈顶元素与当前元素是否匹配，遇到对应的括号出栈
+ 若匹配失败即左右括号数目一样但是不匹配，若字符遍历完毕最终栈不为空则左括号多余，若栈为空字符未遍历完毕则右括号多余
+ 匹配方法1：左括号入栈时直接用对应右括号
+ 匹配方法2：哈希表构建左右括号对应关系：key对应左括号，value对应右括号
+
+ */
+public boolean isValid1(String s) {
+    int i=0;
+    stackin = new ArrayDeque<>();
+//      哈希表构建左右括号对应关系：key对应左括号，value对应右括号
+    HashMap<Character,Character> sites = new HashMap<>();
+    sites.put('(',')');
+    sites.put('{','}');
+    sites.put('[',']');
+
+//      括号落单情况排除
+    if (s.length()%2 != 0){
+        return false;
+    }
+//      遍历字符串s，为了方便匹配括号，将与之对应的括号入栈
+    while (i<s.length()){
+        if (sites.containsKey(s.charAt(i))){
+            stackin.push(s.charAt(i));
+        }
+//          判断无效括号：栈为空或者括号不匹配
+        else if (stackin.isEmpty() || s.charAt(i)!=sites.get(stackin.peek())){
+            return false;
+        }
+//          将匹配成功的括号对消除
+        else if (s.charAt(i)==sites.get(stackin.peek())){
+            stackin.pop();
+        }
+        i++;
+    }
+//      若字符串已遍历完毕，栈仍不为空，则为左括号冗余情况
+    return stackin.isEmpty();
+}
 
 
 
