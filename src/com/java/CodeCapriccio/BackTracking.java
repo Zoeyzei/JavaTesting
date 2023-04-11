9e72e3f7ae1a62962dccb84f327cbc75f4421705
Zoey <827650256@qq.com>
2023-04-11 17:35:16 +0800

    20230411

--- src/com/java/CodeCapriccio/BackTracking.java
+++ src/com/java/CodeCapriccio/BackTracking.java
@@ -34,9 +34,9 @@ void backtracking(参数) {
     输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 
 */
-    public List<List<Integer>> permute(int[] nums) {
-
-    }
+//    public List<List<Integer>> permute(int[] nums) {
+//
+//    }
 
 
 
