6d61b3adfc53d6f557935b7a583d26c11cd56ea5
Zoey <827650256@qq.com>
2023-04-28 17:21:06 +0800

    20230428

---
+++ src/com/java/CodeCapriccio/Simulation.java
@@ -0,0 +1,39 @@
+package com.java.CodeCapriccio;
+
+import java.util.HashMap;
+
+public class Simulation {
+
+    public static void main(String[] args) {
+
+    }
+
+
+/******************************     旋转数字     *************************
+    我们称一个数 X 为好数, 如果它的每位数字逐个地被旋转 180 度后，我们仍可以得到一个有效的，且和 X 不同的数。要求每位数字都要被旋转。
+    如果一个数的每位数字被旋转以后仍然还是一个数字， 则这个数是有效的。0, 1, 8 被旋转后仍然是它们自己；2 和 5  6 和 9 互为镜像；
+    现在我们有一个正整数 N, 计算从 1 到 N 中有多少个数 X 是好数？
+     输入: 10
+     输出: 4
+     解释:
+     在[1, 10]中有四个好数： 2, 5, 6, 9。
+     注意 1 和 10 不是好数, 因为他们在旋转之后不变。
+ 思路: 每位数字用旋转后数字替代,判断是否与原数字相同
+*/
+//    public int rotatedDigits(int n) {
+//        String s = ""+n;
+//        HashMap<Integer,Integer> sites = new HashMap<>();
+//        sites.put(0,0);
+//
+//    }
+
+
+
+
+
+
+
+
+
+
+}
